f31501eb2492eb90507e1b8d7e590b0f60d953d5
Jamie Bryson <jamieab98@gmail.com>
2025-10-30 17:18:23 -0400

    Made it so when I click on a question card, it's styling changes to show the user that it has already been answered.

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -5,19 +5,20 @@ import { useState } from "react";
 function QuestionCards({changeDisplay, displayGrid, changeQuestion, setCorrectAnswer, setDisplayGrid, setPoints}) {
     const [questions, setQuestions] = useState(scienceQuestions);
 
-    function handleClick(question) {
+    function handleClick(question, id) {
         changeDisplay();
         changeQuestion(question);
         setCorrectAnswer(question.answer);
         setDisplayGrid(false);
         setPoints(question.points);
+        setQuestions(prev => prev.map(q => q.id === id ? { ...q, answered: true} : q));
     }
 
     return(
         <div>
             <div className={`${displayGrid ? styles.questionGrid : styles.invisible}`}>
                 {questions.map((question, index) => (
-                    <div className={styles.questionCard} key={index} onClick={() => handleClick(question)}>
+                    <div className={question.answered ? styles.answeredQuestion : styles.questionCard} key={index} onClick={() => handleClick(question, question.id)}>
                         <div>Question Number {question.id}</div>
                         <div>Point Value: {question.points}</div>
                     </div>
